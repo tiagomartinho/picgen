d5fead5d75f0add5b17c4a6036db575ed089d306
Tiago Martinho <t.martinho@icloud.com>
2022-12-02 15:07:55 +0700

    Initial Commit

---
+++ picgen/picgenApp.swift
@@ -0,0 +1,17 @@
+//
+//  picgenApp.swift
+//  picgen
+//
+//  Created by Tiago Martinho on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct picgenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
